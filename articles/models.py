a8021536c5f9c2f8f070ea0e479869adfef47c18
Artem Tkachuk <arti17900@gmail.com>
2019-12-17 18:29:39 +0600

    Добавлен вывод комментариев на странице.

--- articles/models.py
+++ articles/models.py
@@ -17,9 +17,9 @@ class Article(models.Model):
 
 
 class Comment(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
     comment = models.CharField(max_length=140)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, )
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
 
     def __str__(self):
         return self.comment
